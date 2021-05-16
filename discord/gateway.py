2db8c0662e5d8ecd6e26a04bf20463b74df9eb25
Rapptz <rapptz@gmail.com>
2021-05-16 03:51:27 -0400

    Fix sending arrays with nulls in them when changing presences

--- discord/gateway.py
+++ discord/gateway.py
@@ -599,7 +599,7 @@ class DiscordWebSocket:
         if activity is not None:
             if not isinstance(activity, BaseActivity):
                 raise InvalidArgument('activity must derive from BaseActivity.')
-            activity = activity.to_dict()
+            activity = [activity.to_dict()]
 
         if status == 'idle':
             since = int(time.time() * 1000)
@@ -607,7 +607,7 @@ class DiscordWebSocket:
         payload = {
             'op': self.PRESENCE,
             'd': {
-                'activities': [activity],
+                'activities': activity,
                 'afk': afk,
                 'since': since,
                 'status': status
